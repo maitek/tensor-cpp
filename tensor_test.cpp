130598a42383142b9190e692e376737f9220a59f
Mathias Sundholm <mathias_sundholm@hotmail.com>
2018-03-10 12:28:28 +0100

    cleaned up test

--- tensor_test.cpp
+++ tensor_test.cpp
@@ -3,17 +3,12 @@
 
 
 int main(){
-	//float data_a[] = {1,2,3,4,5,6};
-	//Tensor A = Tensor(2,3, data_a);
-  Tensor A = Tensor::randn(4,5);
-	std::cout << "A:" << std::endl << A.toString() << std::endl;
+	float data_a[] = {1,2,3,4,5,6};
+	Tensor A = Tensor(2,3, data_a);
+    std::cout << "A:" << std::endl << A.toString() << std::endl;
 
-	//float data_b[] = {1,2,3,4,5,6};
-	//Tensor B = Tensor(3,2, data_b);
 	Tensor B = A.transpose();
 
-	//std::cout << "A:" << std::endl << A.toString() << std::endl;
-	//Tensor B = ;
 	std::cout << "B:" << std::endl << B.toString() << std::endl;
 
 	Tensor C = A + A;
@@ -24,7 +19,6 @@ int main(){
 
 	Tensor E = B * B;
 	std::cout << "E = B * B:" << std::endl << E.toString() << std::endl;
-
 
 	std::cout << "E(0,0): " << std::to_string(E(0,0)) << std::endl;
 	std::cout << "E(1,0): " << std::to_string(E(1,0)) << std::endl;
